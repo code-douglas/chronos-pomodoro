f06a7a6f22b7ac05d1044f1a38a6b238a0638607
Douglas <douglasd3v.contato@gmail.com>
2025-08-28 12:23:21 -0300

    Reset on CSS and Add type of Heading components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
+import './styles/theme.css';
+import './styles/global.css';
+import { Heading } from './components/Heading';
+import { TimerIcon } from 'lucide-react';
+
 function App() {
   return (
     <div>
-      <h1>Olá, Mundo!</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. In est aut
-        beatae repellat quae nam eius, maxime alias necessitatibus aliquid
-        libero sint consequatur fugiat? Tempora iure repellendus quaerat culpa
-        voluptas.
-      </p>
+      <Heading>
+        Olá
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,5 @@ import { App } from './App';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <h1>Olá, Mundo!</h1>
   </StrictMode>,
 );
