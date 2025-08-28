7a7c71b4857a972b603188e12428088cd4186b05
Douglas <douglasd3v.contato@gmail.com>
2025-08-28 09:19:08 -0300

    New: App.tsx/main.tsx alt for study

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { App } from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <h1>Olá, Mundo!</h1>
   </StrictMode>,
-)
+);
