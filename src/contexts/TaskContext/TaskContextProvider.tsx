f4a0a17368b23f36431c83cf09e7cc7b3e39952c
Douglas <douglasd3v.contato@gmail.com>
2025-09-06 08:45:19 -0300

    Finalizing audio alert

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimerWorkManager } from '../../workers/timerWorkerManage';
 import { TaskActionsTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 export type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -11,12 +12,17 @@ export type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
   const worker = TimerWorkManager.getInstance();
 
   worker.onmessage(e => {
     const countDownSeconds = e.data;
 
     if (countDownSeconds <= 0) {
+      if (playBeepRef.current) {
+        playBeepRef.current();
+        playBeepRef.current = null;
+      }
       dispatch({
         type: TaskActionsTypes.COMPLET_TASK,
       });
@@ -36,6 +42,14 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 
     worker.postMessage(state);
   }, [worker, state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
--- src/contexts/TaskContext/initialTaskState.ts
+++ src/contexts/TaskContext/initialTaskState.ts
@@ -3,7 +3,7 @@ import type { TaskStateModel } from '../../models/TaskStateModel';
 export const initialTaskState: TaskStateModel = {
   tasks: [],
   secondsRemaining: 0,
-  formattedSecondsRemaining: '01:00',
+  formattedSecondsRemaining: '25:00',
   activeTask: null,
   currentCycle: 0,
   config: {
